6c0bb71459e95d28e0256b3eb2800fad8eadd1ff
Akshaya <akshaya123@gmail.com>
2026-08-19 21:03:41 +0530

    my first program

---
+++ sample/src/samplepack/Firstpro.java
@@ -0,0 +1,11 @@
+package samplepack;
+
+public class Firstpro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Eclipse");
+        
+	}
+
+}
